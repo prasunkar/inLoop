5892d00a5c36c7228d2cc67d1a49147fe4eb3b61
Prasun Kar <prasunkar24@gmail.com>
2022-01-22 18:42:59 -0500

    Created SEO component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,13 @@
 module.exports = {
   siteMetadata: {
-      title: `inLoop`,
-    siteUrl: `https://inloop.today`
+    title: `inLoop`,
+    titleTemplate: `%s • inLoop`,
+    description: `Keeping youth in the loop on what is happening around the world, one challenge at a time.`,
+    url: `https://inloop.today`,
+    image: ``,
+    twitterUsername: ``,
+    instagramUsername: `inloop.ja`,
+    linkedinUsername: `inloopja`,
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
